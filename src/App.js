7750863059d09c8d9ff9bd59ea99fc548e07f154
Jarno Luoma-Nirva <jln@iki.fi>
2018-07-25 14:18:47 +0300

    Add CharComponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validator from './ValidationComponent/ValidationComponent.js';
+import CharComponent from './CharComponent/CharComponent.js';
 
 class App extends Component {
   state = {
@@ -20,11 +21,12 @@ class App extends Component {
         <input type="text" onChange={this.textChangedHandler} />
         <p> Length: {this.state.text.length} </p>
         <Validator text={this.state.text} />
+        <CharComponent character="T" />
         <ol>
           <li className="done">Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
           <li className="done">Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
           <li className="done">Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
+          <li className="done">Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
